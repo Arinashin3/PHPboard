1c780b5967e02cf8fadb87547394909737c527e4
arinashin3 <arinashin3@gmail.com>
2022-07-27 21:09:45 +0900

    Arinashin3

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!--자동 로그인-->
 <?php
-    include "./db/dbconn.php";
+    include "/db/dbconn.php";
     session_start();
-    $goto_login = './access/login.php';
-    $goto_board = './board/main.php';
+    $F_LOGIN = '/access/login.php';
+    $S_LOGIN = '/board/main.php';
 
-    if (!isset($_COOKIE['id']) or !isset($_SESSION['id'])) {
-        echo "<meta http-equiv='refresh content='0; url=$goto_login'>";
+    if (!isset($_SESSION['id'])) {
+        echo "<meta http-equiv='refresh' content='0; url=$F_LOGIN'>";
     }
     else {
-        echo "<meta http-equiv='refresh content='0; url=$goto_board'>";
+        echo "<meta http-equiv='refresh' content='0; url=$S_LOGIN'>";
     }
 ?>
